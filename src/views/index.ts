aa075707d4d9e8e76c1df5d3fdb3eab7dc600a98
dallascrichmond <113049138+dallascrichmond@users.noreply.github.com>
2023-07-15 17:49:31 -0700

    Added imports

--- src/views/index.ts
+++ src/views/index.ts
@@ -1,5 +1,13 @@
 import Home from "./Home/Home";
+import About from "./About/About";
+import Resume from "./Resume/Resume";
+import Projects from "./Projects/Projects";
+import Contact from "./Contact/Contact";
 
 export {
   Home,
+  About,
+  Resume,
+  Projects,
+  Contact,
 };
