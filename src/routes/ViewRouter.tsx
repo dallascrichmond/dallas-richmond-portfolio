fd0336d634a54494b2ecdda4f11629ade629ab99
dallascrichmond <113049138+dallascrichmond@users.noreply.github.com>
2023-07-15 17:51:48 -0700

    Added routes

--- src/routes/ViewRouter.tsx
+++ src/routes/ViewRouter.tsx
@@ -3,12 +3,22 @@
  * @author Dallas Richmond
  */
 import { Routes, Route } from 'react-router-dom';
-import { Home } from '../views';
+import { 
+  Home,
+  About,
+  Resume,
+  Projects,
+  Contact,
+ } from '../views';
 
 export default function ViewRouter() {
   return (
     <Routes>
       <Route path='/' Component={Home} />
+      <Route path='/About' Component={About} />
+      <Route path='/Resume' Component={Resume} />
+      <Route path='/Projects' Component={Projects} />
+      <Route path='Contact' Component={Contact} />
     </Routes>
   );
 }
